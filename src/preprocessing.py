11f32157e65bb8e0ab1a71bf0e85c51e2d40ca80
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-11-18 18:10:23 +0800

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,17 @@
+def read_and_decode(filename, reshape_dims):
+    image = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(image, channels=IMG_CHANNELS)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    return tf.image.resize(image, reshape_dims)
+
+def show_image(filename):
+    image = read_and_decode(filename, [IMG_HEIGHT, IMG_WIDTH])
+    plt.imshow(image.numpy());
+    plt.axis('off');
+    
+def decode_csv(csv_row):
+    record_defaults = ['Id', 'Weight']
+    filename, pawpularity = tf.io.decode_csv(csv_row, record_defaults)
+    pawpularity = tf.convert_to_tensor(float(pawpularity), dtype=tf.float32)
+    image = read_and_decode(filename, [IMG_HEIGHT, IMG_WIDTH])
+    return image, pawpularity
