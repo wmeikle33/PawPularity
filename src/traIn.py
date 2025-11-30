62d9d8e84772603e114b729d9260773fb08f7340
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2025-11-30 21:51:03 +0800

    Update traIn.py

--- src/traIn.py
+++ src/traIn.py
@@ -1,1 +1,33 @@
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=1, activation=None)
+])
+
+model.summary()
+
+tf.keras.utils.plot_model(model, show_shapes=True, show_layer_names=False)
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.MeanSquaredError(),
+              metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+%%time
+
+history = model.fit(train_dataset, validation_data=eval_dataset, epochs=25, batch_size=BATCH_SIZE)
+
+def training_plot(metrics, history):
+    f, ax = plt.subplots(1, len(metrics), figsize=(5*len(metrics), 5))
+    for idx, metric in enumerate(metrics):
+        ax[idx].plot(history.history[metric], ls='dashed')
+        ax[idx].set_xlabel('Epochs')
+        ax[idx].set_ylabel(metric)
+        ax[idx].plot(history.history['val_'+metric]);
+        ax[idx].legend(['train_'+metric, 'val_'+metric])
+
+training_plot(['loss', 'root_mean_squared_error'], history)
 
