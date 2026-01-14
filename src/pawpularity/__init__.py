6da12264f4e2e20f07c2234433c64c8f3fd89e3a
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-01-14 10:58:28 +0800

    Update __init__.py

--- src/pawpularity/__init__.py
+++ src/pawpularity/__init__.py
@@ -16,3 +16,17 @@ newdf = pd.DataFrame(data, columns = ['Id', 'Weight'])
 
 data_path = '/Users/wmeikle/Downloads/petfinder-pawpularity-score/'
 data = pd.read_csv(data_path+'train.csv')
+
+sssplit = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
+for train_index, test_index in sssplit.split(data, data['Pawpularity']):
+    training_set = data.iloc[train_index]
+    eval_set = data.iloc[test_index]
+    
+training_set['Pawpularity'].hist(label='Training set')
+eval_set['Pawpularity'].hist(label='Eval set')
+plt.title('Pawpularity score distribution in training and test set')
+plt.xlabel('Pawpularity score')
+plt.ylabel('Count')
+plt.legend(loc='upper right')
+plt.show()
+
