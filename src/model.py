27201a07df4747d86e157922f6bdcb67809f7317
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-02-20 23:40:28 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,19 @@
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=1, activation=None)
+])
 
+model.summary()
+
+tf.keras.utils.plot_model(model, show_shapes=True, show_layer_names=False)
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.MeanSquaredError(),
+              metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+%%time
