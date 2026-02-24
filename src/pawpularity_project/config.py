7620374ba614128ce69a833701d25544608c0240
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-02-25 01:11:34 +0800

    Update config.py

--- src/pawpularity_project/config.py
+++ src/pawpularity_project/config.py
@@ -1,4 +1,14 @@
 
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import matplotlib.pylab as plt
+import os
+import PIL
+from PIL import UnidentifiedImageError
+from sklearn.model_selection import StratifiedShuffleSplit
+pd.options.mode.chained_assignment = None
+
 num_rows = 162
 num_cols = 2
 data = [[None] * num_cols] * num_rows 
