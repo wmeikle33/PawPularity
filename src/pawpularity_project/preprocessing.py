7ecdb0415a1050957fd19d5e1a2ab1362bb6d5da
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-25 19:06:33 +0800

    Update preprocessing.py

--- src/pawpularity_project/preprocessing.py
+++ src/pawpularity_project/preprocessing.py
@@ -1,4 +1,5 @@
-sssplit = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
-for train_index, test_index in sssplit.split(data, data['Pawpularity']):
-    training_set = data.iloc[train_index]
-    eval_set = data.iloc[test_index]
+def stratified_split(data, n_splits, test_size):
+    sssplit = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
+    for train_index, test_index in sssplit.split(data, data['Pawpularity']):
+        training_set = data.iloc[train_index]
+        eval_set = data.iloc[test_index]
